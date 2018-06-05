8132cb4aad4437fac8174764e1d7076524dcf66d
Etienne Guerlais <eguerlais@gmail.com>
2018-06-05 14:01:10 +0200

    Changed flag names and hasher handling

--- cmd/root.go
+++ cmd/root.go
@@ -29,25 +29,34 @@ import (
 )
 
 var (
-	verbose       bool
-	defaultHasher bool
-	force         bool
-	brokers       string
-	remoteBrokers string
-	from          string
-	to            string
-	timeout       int
-	consumerGroup = "kafka-topic-cloner"
+	verbose         bool
+	loop            bool
+	fromBrokers     string
+	toBrokers       string
+	from            string
+	to              string
+	hasher          string
+	timeout         int
+	consumerGroup   = "kafka-topic-cloner"
+	possibleHashers = []string{"murmur2", "FNV-1a"}
 )
 
 const appName = "kafka-topic-cloner"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "kafka-topic-cloner --brokers [url] --from [source] --to [target]",
+	Use:   "kafka-topic-cloner --from-brokers [url] --from [source] --to [target]",
 	Short: "Small cli used to clone the content of a topic into another one",
-	Long:  `Consumes all the events stored in the source topic, and produces them in the target topic. The two topics have to co-exist inside the same cluster.`,
-	Run:   Clone,
+	Long: `
+	Kafka topic cloner consumes all the events stored in the source topic, and produces them in the target topic.
+	The events will see their partitions re-assigned in the process, based on the hasher that you can specify.
+
+	Cloning between two different clusters can be achieved by using the --to-cluster flag.
+
+	Same-topic cloning (also called loop-cloning) is protected by the --loop flag. In this case, the source topic (--from) will be used as both source and target.
+	This can be a risky operation since it will multiply the messages in the source topic until manual interruption, use with caution!
+	`,
+	Run: Clone,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -60,17 +69,16 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose mode")
-	rootCmd.PersistentFlags().BoolVarP(&defaultHasher, "default-hasher", "d", false, "use the default sarama hasher for partitioning instead of murmur2")
-	rootCmd.PersistentFlags().BoolVarP(&force, "force", "F", false, "force cloning into the source topic")
-	rootCmd.PersistentFlags().StringVarP(&brokers, "brokers", "b", "", "semicolon-separated Kafka brokers URLs")
-	rootCmd.PersistentFlags().StringVarP(&remoteBrokers, "remote-brokers", "r", "", "semicolon-separated destination Kafka brokers URLs")
+	rootCmd.PersistentFlags().BoolVarP(&loop, "loop", "L", false, "loop mode (clone into the source topic)")
+	rootCmd.PersistentFlags().StringVarP(&fromBrokers, "from-brokers", "F", "", "address of the source kafka brokers, semicolon-separated")
+	rootCmd.PersistentFlags().StringVarP(&toBrokers, "to-brokers", "T", "", "address of the target kafka brokers, semicolon-separated (specify only if different from the source brokers)")
 	rootCmd.PersistentFlags().StringVarP(&from, "from", "f", "", "source topic")
 	rootCmd.PersistentFlags().StringVarP(&to, "to", "t", "", "target topic")
-	rootCmd.PersistentFlags().IntVarP(&timeout, "timeout", "o", 10000, "delay before timing out")
+	rootCmd.PersistentFlags().StringVarP(&hasher, "hasher", "H", "murmur2", "partitioning hasher (possible values: murmur2, FNV-1a")
+	rootCmd.PersistentFlags().IntVarP(&timeout, "timeout", "o", 10000, "delay (ms) before exiting after the last message has been cloned")
 
-	rootCmd.MarkPersistentFlagRequired("brokers")
+	rootCmd.MarkPersistentFlagRequired("from-brokers")
 	rootCmd.MarkPersistentFlagRequired("from")
-	rootCmd.MarkPersistentFlagRequired("to")
 }
 
 //Clone handles the consuming / producing process
@@ -81,19 +89,19 @@ func Clone(cmd *cobra.Command, args []string) {
 		return
 	}
 
-	b := strings.Split(brokers, ";")
+	b := strings.Split(fromBrokers, ";")
 
 	consumer := kafka.NewConsumer(from, b, consumerGroup)
 	if verbose {
 		log.Printf("consumer (group: %s) initialized on %s/%s", consumerGroup, b, from)
 	}
 
-	if remoteBrokers != "" {
-		b = strings.Split(remoteBrokers, ";")
+	if toBrokers != "" {
+		b = strings.Split(toBrokers, ";")
 	}
-	producer := kafka.NewProducer(b, defaultHasher)
+	producer := kafka.NewProducer(b, hasher)
 	if verbose {
-		log.Printf("producer initialized on %s/%s, default hasher: %t", b, to, defaultHasher)
+		log.Printf("producer initialized on %s/%s, hasher: %s", b, to, hasher)
 	}
 
 	//Try to gracefully shutdown
@@ -144,17 +152,37 @@ Loop:
 
 func validateParameters() error {
 	switch true {
-	case brokers == "":
-		return errors.New("brokers must be set")
+
 	case from == "":
 		return errors.New("source topic must be set")
-	case to == "":
-		return errors.New("target topic must be set")
-	case from == to && !force:
-		return errors.New("same-topic cloning is disabled, use --force to ignore")
-	case brokers == remoteBrokers:
-		return errors.New("source and target brokers are the same, don't use --remote-brokers")
-	}
 
+	case to == "" && !loop:
+		return errors.New("target topic must be set")
+
+	case to != "" && loop:
+		return errors.New("do not specify target topic when loop-cloning")
+
+	case from == to && !loop && toBrokers == "":
+		return errors.New("cannot clone into the same topic without using --loop")
+
+	case fromBrokers == "":
+		return errors.New("source brokers must be set")
+
+	case fromBrokers == toBrokers:
+		return errors.New("source and target brokers are the same")
+
+	case !contains(possibleHashers, hasher):
+		return errors.New("unknown hasher, see help for possible values")
+
+	}
 	return nil
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
